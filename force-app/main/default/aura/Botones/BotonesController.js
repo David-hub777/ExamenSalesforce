0c8151db326743b7d93b886ae26336b0abcfe11d
David <david25042001@gmail.com>
2024-02-19 10:52:37 +0100

    commit nueve

--- force-app/main/default/aura/Botones/BotonesController.js
+++ force-app/main/default/aura/Botones/BotonesController.js
@@ -17,21 +17,48 @@
         evt.setParams({
             "grabando": false
         });
+        var secuenciasActuales = component.get("v.cadenaGrabacion");
+        secuenciasActuales.push(",");
+        component.set("v.cadenaGrabacion", secuenciasActuales);
     },
     grabacion: function(component, event, helper) {
-        
+        var cadenaGrabacion = component.get("v.cadenaGrabacion");
+        for (var i = 0; i < cadenaGrabacion.length; i++) {
+            console.log(cadenaGrabacion[i]);
+        }
     },
     guardar: function(component, event, helper) {
+        var insertCadenaApex = component.get("c.insertCadena"); 
+        var cadenaGrabacion = component.get("v.cadenaGrabacion");
+        
+        insertCadenaApex.setCallback(this, function(response){
+            var state = response.getState();
+            if(state === "SUCCESS"){
+                alert("Exito al guardar")
+            }
+        });
+
+        for (var i = 0; i < cadenaGrabacion.length; i++) {
+            insertCadenaApex.setParams({
+                "cadena": cadenaGrabacion[i],
+            });
+            $A.enqueueAction(insertCadenaApex);
+        }
         
     },
     borrarPantalla: function(component, event, helper) {
-        
+        var cadenaGrabacion = component.get("v.cadenaGrabacion");
+        cadenaGrabacion.splice(0, cadenaGrabacion.length);
+        component.set("v.cadenaGrabacion", cadenaGrabacion);
     },
     manejarEvento: function(component, event, helper) {
         var valorPasar = event.getParam("pasar");
+        var secuenciasActuales = component.get("v.cadenaGrabacion");
+        if (event.getParam("v.grabar")) {
+            secuenciasActuales.push(valorPasar);
+            component.set("v.cadenaGrabacion", secuenciasActuales);
+        }
         console.log('valorPasar: ' + valorPasar);
-        // Establecer el valor de 'cadenaGrabacion' con el valor de 'pasar'
-        component.set("v.cadenaGrabacion", valorPasar);
     }
 
 })
