05b960e5b375f64089b67943c15ec633d1c6f8e0
David <david25042001@gmail.com>
2024-02-19 09:10:37 +0100

    tercer commit

--- force-app/main/default/aura/ceroUno/ceroUnoController.js
+++ force-app/main/default/aura/ceroUno/ceroUnoController.js
@@ -2,11 +2,26 @@
     myAction : function(component, event, helper) {
 
     },
-    manejarEvento : function(component, event, helper) {
-        var pointsToAdd = event.getParam("pasar");
-        var valorPasado = component.get("v.valorPasado");
-        pointsToAdd = valorPasado;
+    manejarEvento0 : function(component, event, helper) {
+        var evt = $A.get("e.c:Evento");
+        var valorExistenete = evt.getParam("pasar");
+        console.log("evt: " + evt);
+        evt.setParams({
+            "pasar": valorExistenete + 0 
+        });
+        evt.fire();
         
-        component.set("v.valorPasado", valorPasado);
-    }
+        // event.set("v.valorEvento", valorPasado);
+    },
+    manejarEvento1 : function(component, event, helper) {
+        var evt = $A.get("e.c:Evento");
+        var valorExistenete = evt.getParam("pasar");
+        evt.setParams({
+            "pasar": valorExistenete + 1 
+        });
+        console.log("Evento Value: " + evt);
+        evt.fire();
+        
+        // event.set("v.valorEvento", valorPasado);
+    },
 })
