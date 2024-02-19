8ffc7fbe36b67d9f7e6f9cbcde680e30f038994c
David <david25042001@gmail.com>
2024-02-19 08:52:57 +0100

    segundo commit

--- force-app/main/default/aura/ceroUno/ceroUnoController.js
+++ force-app/main/default/aura/ceroUno/ceroUnoController.js
@@ -1,5 +1,12 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    manejarEvento : function(component, event, helper) {
+        var pointsToAdd = event.getParam("pasar");
+        var valorPasado = component.get("v.valorPasado");
+        pointsToAdd = valorPasado;
+        
+        component.set("v.valorPasado", valorPasado);
     }
 })
